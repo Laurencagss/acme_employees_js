e2e29aa6e74e83878e7fd907c5f87b60c3dc9d41
Lauren Cagnetta <laurenscolleges@gmail.com>
2022-01-25 17:28:53 -0500

    first

---
+++ empoyees2.js
@@ -0,0 +1,18 @@
+function findEmployeeByName(name,arr){
+    for (let i = 0; i < arr.length;i++){
+        let id = arr.length[i];
+    return name + id; 
+}
+}
+
+
+
+function findManagerFor(name,arr,arr2){
+    for (let i = 0; i < arr.length;i++){
+        let id = arr.length[i];
+ for (let j = 0; j < arr2.length; j++){
+     let managerId = arr2.length[i];
+ }
+    }
+  }
+
